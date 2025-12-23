2a8bb3998cbb4ca8e238a9e95029be3caab912e7
rawsn <32265306+rawsn@users.noreply.github.com>
2025-12-23 11:59:30 +0100

    Delete server directory

--- server/rules/utils/textParser.js
+++
@@ -1,8 +0,0 @@
-import { load } from "cheerio";
-
-// Vereenvoudigde HTML-parser
-export function parseTextFromHTML(html) {
-  const $ = load(html);
-  const text = $("p, li").text();
-  return text.replace(/\s+/g, " ").trim();
-}
