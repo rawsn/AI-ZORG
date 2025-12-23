84f5f3a2f9cd2149e7e381c726d8c3ac59498c48
rawsn <32265306+rawsn@users.noreply.github.com>
2025-12-23 11:22:27 +0100

    Delete utils directory

--- utils/meldplichtCheck.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-export function checkMeldplicht(klacht) {
-  const file = path.join(process.cwd(), "data", "rules", "meldplicht.json");
-  const data = JSON.parse(fs.readFileSync(file, "utf8"));
-  const key = Object.keys(data).find(k => klacht.toLowerCase().includes(k));
-  return key ? data[key] : null;
-}
--- utils/routingEngine.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import path from "path";
-export function getZorgToegang(route) {
-  const file = path.join(process.cwd(), "data", "rules", "zorgtoegang.json");
-  const data = JSON.parse(fs.readFileSync(file, "utf8"));
-  return data[route] || { route: "huisarts", verwijzing: true };
-}
